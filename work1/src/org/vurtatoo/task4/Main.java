edbb33375aaa86b2d2adaea0c22f2423afa0b3c4
Andrei Vurtatov <andrei@vurtatoo.tk>
2014-09-08 22:25:36 +0300

    add task4

---
+++ work1/src/org/vurtatoo/task4/Main.java
@@ -0,0 +1,43 @@
+package org.vurtatoo.task4;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String folder = scanner.nextLine();
+		scanner.close();
+		File file = new File(folder);
+		if (file.isDirectory()) {
+			Map<String, Object> map = getMap(file);
+			System.out.println(map);
+		} else {
+			System.err.println("IS NOT DIRECTORY");
+		}
+		
+
+	}
+
+	private static Map<String, Object> getMap(File fileSource) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		List<String> files = new ArrayList<String>();
+		for (String fileString : fileSource.list()) {
+			File file  = new File(fileSource.getAbsolutePath() + System.getProperty("file.separator") +fileString);
+			//System.out.println(file.getAbsolutePath());
+			if (file.isDirectory()) {
+				map.put(fileString, getMap(file));
+			} else {
+				files.add(fileString);
+			}
+		}
+		map.put("files", files);
+		return map;
+	}
+
+}
